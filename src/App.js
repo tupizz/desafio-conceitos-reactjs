d6650993eee9e4b6cf62a568812d47bf185205a6
Tadeu Tupinamba <tadeu.tupinamba@IBOOK-80075.local>
2020-06-18 16:27:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import api from './services/api';
+
+import './styles.css';
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((res) => setRepositories(res.data));
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Projeto ${Date.now()}`,
+      owner: 'Tadeu Tupinambá',
+    });
+
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter((item) => item.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repositorie) => (
+          <li key={repositorie.id}>
+            {repositorie.title}
+
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
